eb9b5f738b090cbd22de783cde0af8110ab35bd9
abuzaramer38-blip <abuzaramer38@gmail.com>
2026-04-03 03:59:56 +0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -3,27 +3,27 @@
 
 const Physics = (() => {
   let world = null;
-  const bodies = [];
   
   function init() {
+    if (world) return world; // Agar pehle se bana hai toh wahi dedo
+    
     world = new CANNON.World();
-    if (world) {
-      world.gravity.set(0, -20, 0);
-      world.broadphase = new CANNON.SAPBroadphase(world);
-      world.solver.iterations = 20;
-      world.defaultContactMaterial.friction = 0.4;
-      world.defaultContactMaterial.restitution = 0.1;
-      
-      const groundMat = new CANNON.Material('ground');
-      const bikeMat   = new CANNON.Material('bike');
-      const cm = new CANNON.ContactMaterial(groundMat, bikeMat, {
-        friction: 0.6,
-        restitution: 0.05
-      });
-      world.addContactMaterial(cm);
-      world._groundMat = groundMat;
-      world._bikeMat = bikeMat;
-    }
+    world.gravity.set(0, -20, 0);
+    world.broadphase = new CANNON.SAPBroadphase(world);
+    world.solver.iterations = 20;
+    
+    // Materials setup
+    const groundMat = new CANNON.Material('ground');
+    const bikeMat   = new CANNON.Material('bike');
+    const cm = new CANNON.ContactMaterial(groundMat, bikeMat, {
+      friction: 0.6,
+      restitution: 0.05
+    });
+    world.addContactMaterial(cm);
+    
+    world._groundMat = groundMat;
+    world._bikeMat = bikeMat;
+    
     return world;
   }
 
@@ -32,7 +32,7 @@ const Physics = (() => {
   }
 
   function createBikeBody(pos) {
-    if (!world) init(); // Safety check
+    if (!world) init(); 
     const body = new CANNON.Body({ mass: 220, material: world._bikeMat });
     body.addShape(new CANNON.Box(new CANNON.Vec3(0.35, 0.3, 0.9)));
     body.position.set(pos.x, pos.y, pos.z);
@@ -40,5 +40,14 @@ const Physics = (() => {
     return body;
   }
 
-  return { init, step, createBikeBody, getWorld: () => world };
+  function createStaticBox(hw, hh, hd, pos) {
+    if (!world) init();
+    const body = new CANNON.Body({ mass: 0 });
+    body.addShape(new CANNON.Box(new CANNON.Vec3(hw, hh, hd)));
+    body.position.set(pos.x, pos.y, pos.z);
+    world.addBody(body);
+    return body;
+  }
+
+  return { init, step, createBikeBody, createStaticBox, getWorld: () => world };
 })();
